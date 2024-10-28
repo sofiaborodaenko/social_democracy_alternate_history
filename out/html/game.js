3e83db52638c2f52134c3bd08b79185e50d625e2
Sofia Borodaenko <soph.bor06@gmail.com>
2024-10-28 16:38:42 -0400

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "Social Democracy: An Alternate History" + '_' + "Sofia Borodaenko";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
